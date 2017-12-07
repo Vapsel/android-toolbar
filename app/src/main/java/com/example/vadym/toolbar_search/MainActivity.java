fa6efcf13d41e9dd2e7afec7342cebad06ecfcda
Vadym Perepeliak <vapsel@gmail.com>
2017-12-07 01:51:04 +0100

    Provide comment to complete tasks

--- app/src/main/java/com/example/vadym/toolbar_search/MainActivity.java
+++ app/src/main/java/com/example/vadym/toolbar_search/MainActivity.java
@@ -1,14 +1,10 @@
 package com.example.vadym.toolbar_search;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.SearchView;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -24,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setSupportActionBar((Toolbar) findViewById(R.id.list_toolbar));
+        // TODO add toolbar to the activity. See AppCompatActivity#setSupportActionBar
 
         bookListView = findViewById(R.id.bookListView);
         bookAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, getDataList());
@@ -33,48 +29,17 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.book_list_menu, menu);
+        // TODO load menu from the XML file (book_list_menu.xml)
 
-        final MenuItem searchItem = menu.findItem(R.id.actionSearch);
-        final SearchView searchView = (SearchView) searchItem.getActionView();
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-
-                bookAdapter.getFilter().filter(query);
-                // Reset SearchView
-                searchView.clearFocus();
-                searchView.setQuery("", false);
-                searchView.setIconified(true);
-                searchItem.collapseActionView();
-                // Set activity title to search query
-                MainActivity.this.setTitle(query);
-                return true;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String s) {
-                bookAdapter.getFilter().filter(s);
-                return true;
-            }
-        });
+        // TODO Add SearchView.OnQueryTextListener to the search item in the menu
+        // TODO use bookAdapter to implement search feature
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        if (item.getItemId() == R.id.nightModeCheck) {
-            item.setChecked(!item.isChecked());
-            LinearLayout mainLayout = findViewById(R.id.mainLayout);
-            if (item.isChecked()){
-                mainLayout.setBackgroundColor(Color.GRAY);
-            } else {
-                mainLayout.setBackgroundColor(Color.WHITE);
-            }
-        }
+        // TODO Set checkbox here; Night mode can be simulated as background color.
         return super.onOptionsItemSelected(item);
     }
 
